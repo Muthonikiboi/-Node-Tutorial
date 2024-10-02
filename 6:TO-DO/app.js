e20f60a75ee973589d41b62fa771fa0465d367b3
Muthonikiboi <kiboijoye254@gmail.com>
2024-10-02 16:10:49 +0300

    ADD:Simple TO-DO

---
+++ 6:TO-DO/app.js
@@ -0,0 +1,12 @@
+import chalk from 'chalk';
+import http from 'http';
+import morgan from 'morgan';
+
+//How to use chalk debugging
+// console.log(chalk.blue('This is a debug message'));
+// console.log(chalk.red('This is an error message'));
+// console.log(chalk.yellow('This is a warning message'));
+// console.log(chalk.green('This is a success message'));
+
+console.log(chalk.yellowBright("Lets start building a TO-Do list using crud...🗒🖊"));
+//Create an HTTP server
